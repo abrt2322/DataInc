f66967541bdd66c17f5e2f63f7d7ca47f141c0bb
abrt2322 <rtsoccer048@gmail.com>
2019-11-11 16:16:35 +0900

    水増し最終形態

--- ImageDataGenerator.py
+++ ImageDataGenerator.py
@@ -20,18 +20,18 @@ def show_imgs(imgs, row, col):
         plt.imshow(img)
     plt.show()
 
-    datagen = ImageDataGenerator(
+datagen = ImageDataGenerator(
     rotation_range=30,
     width_shift_range=0.2,
     height_shift_range=0.2,
     horizontal_flip=True,
     channel_shift_range=100
-    )
+)
 
-    max_img_num = 16
-    imgs =[]
-    for d in datagen.flow(x , batch_size = 1):
-        imgs.append(image.array_to_img(d[0], scale=True))
-        if (len(imgs) % max_img_num) == 0:
-            break
-    show_imgs(imgs, row=4, col=4)
+max_img_num = 16
+imgs =[]
+for d in datagen.flow(x , batch_size = 1):
+    imgs.append(image.array_to_img(d[0], scale=True))
+    if (len(imgs) % max_img_num) == 0:
+        break
+show_imgs(imgs, row=4, col=4)
