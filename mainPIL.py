9c3863deb0ed04489aecc5379581c94398fa03fa
abrt2322 <rtsoccer048@gmail.com>
2019-12-01 14:47:19 +0900

    車・廊下の水増し未完成 画像の変換方法が未確定

--- mainPIL.py
+++ mainPIL.py
@@ -1,7 +1,14 @@
 from keras.preprocessing.image import load_img, img_to_array, ImageDataGenerator, array_to_img
 import matplotlib.pyplot as plt
+import os
 
-IMAGE_FILE = "./sample.jpg"
+DATA_DIR = './'  # データディレクトリ
+IMAGE_FILE = 'sample.jpg'  # 対象画像ファイル
+SAVE_DIR = os.path.join(DATA_DIR, 'preview3')  # 生成画像の保存先ディレクトリ
+
+
+if not os.path.exists(SAVE_DIR):
+    os.makedirs(SAVE_DIR)
 
 # 画像をロード（PIL形式画像）
 img = load_img(IMAGE_FILE)
@@ -27,17 +34,16 @@ datagen = ImageDataGenerator(rotation_range=90)
 
 # generatorから9個の画像を生成
 # 今回は1枚のみなのでbatch_sizeは1
-g = datagen.flow(x, batch_size=1)
-for i in range(4):
+g = datagen.flow(x, batch_size=1, save_to_dir=SAVE_DIR, save_prefix='sample', save_format='jpg')
+for i in range(16):
     batches = g.next()
 
     # （1,縦サイズ, 横サイズ, チャンネル数)
     print(batches.shape)
-
     # 画像として表示するため、４次元から3次元データにし、配列から画像にする。
     gen_img = array_to_img(batches[0])
 
-    plt.subplot(2, 2, i + 1)
+    plt.subplot(4, 4, i + 1)
     plt.imshow(gen_img)
     plt.axis('off')
 
